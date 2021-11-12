e68b47f635b49633252bd9bfcbded5d569524ee0
regis <regiscorblin@yahoo.fr>
2021-11-12 21:25:02 +0100

    Add card button logic

--- memo_board.py
+++ memo_board.py
@@ -1,15 +1,16 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from widgets import card
+from widgets import Card, Turn, Game
 from kivy.utils import platform
 import os, shutil, random
 from PIL import Image,ExifTags
 
 class GridPageWidget(BoxLayout):
 
-    def __init__(self,pairs=12):
+    def __init__(self,pairs=5):
         super().__init__()
+        self.game = Game()
         self.pairs = pairs
         self.grid_size = pairs * 2
         self.pool_of_cards = []
@@ -38,32 +39,33 @@ class GridPageWidget(BoxLayout):
             #write function to change size
             pass
 
-        i = 0
-        while i < self.pairs:
+        nb_of_pairs_created = 0
+        while nb_of_pairs_created < self.pairs:
             pick = random.choice(pic_folder)
             if pick not in self.pool_of_cards and pick[-4:].lower()=='.jpg':
                 self.pool_of_cards.append(pick)
-                self.get_picture_from_phone(pick)
-                i+=1
-        #self.pb = progress_bar_func(self.size_game,'Please wait. Pictures are being selected and resized',self.get_picture_from_phone)
+                self.pool_of_cards.append(pick)
+                self.resize_and_save_pictures(pick)
+                nb_of_pairs_created += 1
+        #self.pb = progress_bar_func(self.size_game,'Please wait. Pictures are being selected and resized',self.resize_and_save_pictures)
 
-    def get_picture_from_phone(self, pic):
+    def resize_and_save_pictures(self, pic: str):
 
         print(f'saving {pic}...')
-        foo = Image.open(self.folder + pic)
+        img = Image.open(self.folder + pic)
         # Get Image orientation
-        exif = foo._getexif()
+        exif = img._getexif()
         for tag, value in exif.items():
             decoded = ExifTags.TAGS.get(tag, tag)
             if decoded == 'Orientation':
                 print(value)
                 if value == 6:
                     print(f'{pic} is vertical')
-                    foo = foo.transpose(Image.ROTATE_270)
+                    img = img.transpose(Image.ROTATE_270)
                 elif value == 8:
                     print(f'{pic} is vertical')
-                    foo = foo.transpose(Image.ROTATE_90)
-        foo.save("images_resized/" + pic, quality=95)
+                    img = img.transpose(Image.ROTATE_90)
+        img.save("images_resized/" + pic, quality=95)
         # print(f'{[self.image_selection[self.i]]} is saved')
         # self.i += 1
         # self.pb.progress_bar.value += 1
@@ -73,31 +75,41 @@ class GridPageWidget(BoxLayout):
     #     self.create_grid()
 
     def draw_grid(self):
+        """
+        Using a list of possible grid sizes,
+        calculate the size needed based on the number of pairs
+        Ex: If 10 pairs, we need 20 cards that will fit in a 25 squares grid.
+        """
         
-        # Using a list of possible grid sizes,
-        # calculate the size needed based on the number of pairs
-        # Ex: If 10 pairs, we need 20 cards that will fit in a 25 squares grid.
-
         self.select_pictures()
 
         grids = [4, 9, 16, 25, 36, 49, 64]
-        max_grid = [int(grids[i+1] **0.5) for i, x in enumerate(grids)
-                if (self.grid_size> x and self.grid_size <grids[i+1])][0]
+        max_grid = [int(grids[i+1] ** 0.5) for i, x in enumerate(grids)
+                    if (self.grid_size > x and self.grid_size < grids[i+1])][0]
 
         cards = []
 
         for row in range(max_grid):
             h_layout = BoxLayout(orientation='vertical')
             for col in range(max_grid):
-                button_img = card('images/img1.jpg')
-                #button_img.no_card()
-                #button_img.pair_found()
-                button_img.card_face_down()
-                cards.append(button_img)
-                h_layout.add_widget(button_img)
+                try:
+                    i = random.randrange(len(self.pool_of_cards))
+                    img = self.pool_of_cards.pop(i)
+                    button_img = Card(img, self.game)
+                    #button_img.no_card()
+                    #button_img.pair_found()
+                    button_img.card_face_down()
+                    cards.append(button_img)
+                    h_layout.add_widget(button_img)
+                except:
+                    button_img = Card(None, self.game)
+                    button_img.no_card()
+                    self.game.cards.append(button_img)
+                    h_layout.add_widget(button_img)
             self.add_widget(h_layout)
-        for mame in cards:
-            print (mame)
+            turn = Turn()
+        # for mame in cards:
+        #     print (mame)
 
 
 
--- widgets.py
+++ widgets.py
@@ -1,7 +1,14 @@
 from kivy.uix.button import Button
 from kivy.uix.image import Image as kivImg
+from typing import Optional
 
-class card(Button):
+class Game:
+    
+    def __init__(self):
+        self.cards: Card = []
+        self.turn = Turn()
+
+class Card(Button):
     """ Representing the state of square on the memory board.
     It can be of 4 kinds:
     - Disabled button grey background with no memory card associated.
@@ -9,9 +16,13 @@ class card(Button):
     - Disabled button green background for found pairs
     - Enabled button with blue background for memory card to be returned face up.
     """
-    def __init__(self,img):
+    def __init__(self,img:Optional[str], game):
         super().__init__()
         self.img = img
+        self.found = False
+        self.returned = False
+        self.last_time_returned = 0  # When was the last turn where this card was returned. 0 = not returned yet
+        self.game = game
 
     def no_card(self):
         self.background_color = [0, 0, 0, 0]
@@ -26,9 +37,27 @@ class card(Button):
         self.disabled = False
 
     def card_face_up(self,dt):
-        widget = get_img_for_btn(self.img,self)
-        self.add_widget(widget)
+        self.widget = get_img_for_btn(self.img,self)
+        self.add_widget(self.widget)
         self.disabled = True
+        game_turn = self.game.turn
+        if game_turn.card_returned == 0:
+            game_turn.card_returned += 1
+            game_turn.first_card = self
+             
+        elif game_turn.card_returned == 1:
+            game_turn.card_returned += 1
+            game_turn.second_card = self
+            game_turn.first_card.remove_widget(game_turn.first_card.widget)
+            game_turn.second_card.remove_widget(game_turn.second_card.widget)
+            if game_turn.first_card.img == game_turn.second_card.img:
+                game_turn.first_card.pair_found()
+                game_turn.second_card.pair_found()
+                game_turn.pair_found = True
+            else:
+                game_turn.first_card.card_face_down()
+                game_turn.second_card.card_face_down()
+            game_turn.reset_turn()
 
     def pair_found(self):
         self.background_color = [0, 1, 1, 1]
@@ -43,8 +72,25 @@ def get_img_for_btn(img, btn):
     :param btn: Kivy Button object
     :return: Kivy Image object
     """
-    img_for_btn = kivImg(source=img,
+    image_path = f'images_resized/{img}'
+    img_for_btn = kivImg(source=image_path,
           y=btn.y + (btn.height - btn.height / 3 * 2) / 2 ,
           x=btn.x + (btn.width - btn.width / 3 * 2) / 2,
           size=(btn.width / 3 * 2, btn.height / 3 * 2))
     return img_for_btn
+
+class Turn:
+    
+    def __init__(self):
+        self.player = 1
+        self.card_returned = 0  # 0: no card returned, 1: one card returned, 2: both cards returned
+        self.first_card = None  
+        self.second_card = None
+        self.pair_found = False
+        
+    def reset_turn(self):
+        self.card_returned = 0  # 0: no card returned, 1: one card returned, 2: both cards returned
+        self.first_card = None
+        self.second_card = None
+        self.pair_found = False
+        
